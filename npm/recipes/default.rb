a791a6d15afcff15ee496469539ac1f863041371
Christopher Roussel <christopher@impleri.net>
2015-03-23 16:52:56 -0500

    Add an if statement around executing npm

--- npm/recipes/default.rb
+++ npm/recipes/default.rb
@@ -1,7 +1,9 @@
 include_recipe 'deploy'
 
-node[:deploy].each do |application, deploy|
-  execute "/usr/local/bin/npm install --production" do
-    cwd "#{deploy[:deploy_to]}/current"
+if File.exists?("#{deploy[:deploy_to]}/current/package.json")
+  node[:deploy].each do |application, deploy|
+    execute "/usr/local/bin/npm install --production" do
+      cwd "#{deploy[:deploy_to]}/current"
+    end
   end
 end
