e1421253e4037cd04d9e03fca050006bf635d00d
Christopher Roussel <christopher@impleri.net>
2015-03-23 17:05:52 -0500

    Double loop not needed

--- npm/recipes/install.rb
+++ npm/recipes/install.rb
@@ -3,11 +3,8 @@
 #
 
 node[:deploy].each do |application, deploy|
-  if File.exists?("#{deploy[:deploy_to]}/current/package.json")
-    node[:deploy].each do |application, deploy|
-      execute "/usr/local/bin/npm install --production" do
-        cwd "#{deploy[:deploy_to]}/current"
-      end
-    end
+  execute "/usr/local/bin/npm install --production" do
+    only_if { File.exists?("#{deploy[:deploy_to]}/current/package.json") }
+    cwd "#{deploy[:deploy_to]}/current"
   end
 end
