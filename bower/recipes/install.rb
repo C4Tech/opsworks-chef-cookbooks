a86f8621f72362e648a4b00da47cf1a5eae973cc
Christopher Roussel <christopher@impleri.net>
2015-03-23 17:17:51 -0500

    Run npm and bower as user

--- bower/recipes/install.rb
+++ bower/recipes/install.rb
@@ -5,6 +5,7 @@
 node[:deploy].each do |application, deploy|
   execute "/usr/local/bin/bower install --production" do
     only_if { File.exists?("#{deploy[:deploy_to]}/current/bower.json") }
+    user deploy[:user]
     cwd "#{deploy[:deploy_to]}/current"
   end
 end
--- npm/recipes/install.rb
+++ npm/recipes/install.rb
@@ -5,6 +5,7 @@
 node[:deploy].each do |application, deploy|
   execute "/usr/local/bin/npm install --production" do
     only_if { File.exists?("#{deploy[:deploy_to]}/current/package.json") }
+    user deploy[:user]
     cwd "#{deploy[:deploy_to]}/current"
   end
 end
