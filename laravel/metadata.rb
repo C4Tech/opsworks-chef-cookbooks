6141c1a1e829c0453b59fbfdd43582a35c43e9d0
Christopher Roussel <christopher@impleri.net>
2015-03-23 17:34:12 -0500

    Adding a layer for laravel

---
+++ laravel/metadata.rb
@@ -0,0 +1,7 @@
+name              "laravel"
+maintainer        "C4 Tech and Design"
+maintainer_email  "info@c4tech.com"
+license           "MIT"
+description       "Installs or Updates Laravel application."
+version           "1.0.0"
+
