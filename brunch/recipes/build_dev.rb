bcd63adc530068bea41207aa9a8c55c7f2ae7a1f
Christopher Roussel <christopher@impleri.net>
2015-04-08 15:47:12 -0500

    Handle environment variables for building frontend

--- brunch/recipes/build_dev.rb
+++ brunch/recipes/build_dev.rb
@@ -1,8 +1,13 @@
 #
 node[:deploy].each do |application, deploy|
-   execute "brunch_build" do
+    deploy[:brunch_variables].each do |key, value|
+        Chef::Log.info("Setting ENV[#{key}] to #{value}")
+        ENV[key] = value
+    end
+
+    execute "brunch_build" do
         command "brunch build"
         cwd "#{deploy[:deploy_to]}/current"
         only_if { File.exists?("#{deploy[:deploy_to]}/current/brunch-config.coffee") }
-   end
+    end
 end
