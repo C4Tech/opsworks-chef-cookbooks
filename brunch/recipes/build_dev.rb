38681cb3044811a11c7cba2435f1002c7a7b4ffe
Christopher Roussel <christopher@impleri.net>
2015-04-08 15:03:31 -0500

    Updating brunch recipes

--- brunch/recipes/build_dev.rb
+++ brunch/recipes/build_dev.rb
@@ -1,7 +1,7 @@
 #
 node[:deploy].each do |application, deploy|
    execute "brunch_build" do
-        command "brunch build -P"
+        command "brunch build"
         cwd "#{deploy[:deploy_to]}/current"
         only_if { File.exists?("#{deploy[:deploy_to]}/current/brunch-config.coffee") }
    end
