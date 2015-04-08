8e0f66ba8f71e5542a7b25bd466989f3e2bedb40
Christopher Roussel <christopher@impleri.net>
2015-04-08 14:10:00 -0500

    Update bower::update to match bower::install

--- bower/recipes/update.rb
+++ bower/recipes/update.rb
@@ -3,8 +3,10 @@
 # http://docs.aws.amazon.com/opsworks/latest/userguide/gettingstarted.walkthrough.photoapp.3.html
 #
 node[:deploy].each do |application, deploy|
-   execute "bower_install" do
-        command "bower update"
+   execute "bower_update" do
+        user deploy[:user]
+        command "bower update --production"
         cwd "#{deploy[:deploy_to]}/current"
+        only_if {::File.exists?('./bower.json') }
    end
 end
