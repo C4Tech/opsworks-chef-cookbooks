9586f9d8593a9b490109940a65226229fcaf2c5e
Christopher Roussel <christopher@impleri.net>
2015-04-08 14:40:27 -0500

    Add group for good luck

--- npm/recipes/install.rb
+++ npm/recipes/install.rb
@@ -2,6 +2,7 @@
 node[:deploy].each do |application, deploy|
    execute "npm_install" do
         user deploy[:user]
+        group deploy[:group]
         command "npm install --production"
         cwd "#{deploy[:deploy_to]}/current"
         only_if {::File.exists?("#{deploy[:deploy_to]}/current/package.json") }
