72cc0d8a93b7c004e89d6814ce3e23bf8f77fb81
Christopher Roussel <christopher@impleri.net>
2015-05-01 18:18:07 -0500

    Update Chef logging

--- phpenv/recipes/dotenv_env_vars.rb
+++ phpenv/recipes/dotenv_env_vars.rb
@@ -1,7 +1,7 @@
 #  Custom Environment Variables are set as shown below:
-# 
-# 
-# 
+#
+#
+#
 # {
 #      "deploy": {
 #       "myapp": {
@@ -15,24 +15,24 @@
 
 
 node[:deploy].each do |application, deploy|
-  
-  template "#{deploy[:deploy_to]}/shared/.env" do
+
+  template "#{deploy[:deploy_to]}/current/.env" do
     source "dotenv_variables.php.erb"
-    owner deploy[:user] 
+    owner deploy[:user]
     group deploy[:group]
     mode "0666"
 
-    variables( 
-        :env => (node[:custom_env] rescue nil), 
+    variables(
+        :env => (node[:custom_env] rescue nil),
         :environment => (node[:custom_env][application.to_s][:environment] rescue nil),
-        :application => "#{application}" 
+        :application => "#{application}"
     )
 
-    Chef::Log.info("Generating dotenv for app: #{application} with env: #{custom_env}...")
-   
-    
+    Chef::Log.info("Generating dotenv for app: #{application} with env: #{node[:custom_env][application.to_s][:environment]}...")
+
+
     only_if do
-     File.directory?("#{deploy[:deploy_to]}/shared/}")
+     File.directory?("#{deploy[:deploy_to]}/current/}")
     end
   end
 end
